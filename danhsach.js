10ccdde74f7d40a25194741344a8f3178713c1ae
Kitoess <nammkhanhnguyen@gmail.com>
2024-11-27 21:07:32 +0700

    Add files via upload

---
+++ danhsach.js
@@ -0,0 +1,55 @@
+function Danhsachtv() {
+    const thanhvien = [
+        { hovaten: "Nguyễn Nam Khánh", gioitinh: "Nam", Noisinh: "Hà Nội" },
+        { hovaten: "Trần Đình Hưng Thịnh", gioitinh: "Nam", Noisinh: "Nghệ An" },
+        { hovaten: "Trần Minh Long", gioitinh: "Nam", Noisinh: "Nam Định" },
+        { hovaten: "Đào Thế Vũ", gioitinh: "Nam", Noisinh: "Hà Nội" },
+        { hovaten: "Nguyễn Quang Huy", gioitinh: "Nam", Noisinh: "Hà Tĩnh" },
+        { hovaten: "Nguyễn Công Mạnh", gioitinh: "Nam", Noisinh: "Hà Tĩnh" },
+        { hovaten: "Nguyễn Việt Dương", gioitinh: "Nam", Noisinh: "Hà Nội" },
+    ];
+ 
+    const danhSach = document.getElementById("danhsachthanhvien");
+ 
+    thanhvien.forEach((tv) => { 
+        const div = document.createElement("div");
+        div.className = "thanhvien";
+        div.innerHTML = `
+            <table>
+                <tr>
+                    <td>Họ và tên:</td>
+                    <td>${tv.hovaten}</td>
+                </tr>
+                <tr>
+                    <td>Giới tính:</td>
+                    <td>${tv.gioitinh}</td>
+                </tr>
+                <tr>
+                    <td>Nơi sinh:</td>
+                    <td>${tv.Noisinh}</td>
+                </tr>
+            </table>
+        `;
+ 
+        div.style.margin = "20px";
+        div.style.padding = "10px"; 
+        div.style.background = "lightgray"; 
+        danhSach.appendChild(div); 
+    });
+ }
+function kss()
+{
+var sum=0,count=0;
+for(var icount=0;icount<10;icount++)
+ {
+    sum=sum+icount;
+    count++;
+ }
+ document.getElementById("result").innerHTML=sum;
+}
+Danhsachtv();
+kss();
+for (var i=0;i<thanhvien.length;i++)
+{
+    console.log(thanhvien[i].hovaten);
+}
